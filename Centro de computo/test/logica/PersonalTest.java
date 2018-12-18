2199d77f6f32e84a44c91140b067dda7309ceef2
Paola Maraí <maraibenitez@hotmail.com>
2018-12-18 16:09:29 -0600

    Pruebas

---
+++ Centro de computo/test/logica/PersonalTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author marai
+ */
+public class PersonalTest {
+
+  public PersonalTest() {
+  }
+  
+  @BeforeClass
+  public static void setUpClass() {
+  }
+  
+  @AfterClass
+  public static void tearDownClass() {
+  }
+  
+  @Before
+  public void setUp() {
+  }
+  
+  @After
+  public void tearDown() {
+  }
+}
